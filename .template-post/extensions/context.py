5d6db3901de43b22c37eac7072f397e670f07b15
KuligKamil <kamil.marek.kulig@gmail.com>
2024-12-11 01:00:27 +0100

    chore: fix copier helper

--- .template-post/extensions/context.py
+++ .template-post/extensions/context.py
@@ -3,7 +3,6 @@ from datetime import datetime
 
 class ContextUpdater(ContextHook):
     def hook(self, context: dict) -> dict:
-        print(context)
         if context.get("date") == '':
             context["date"] = datetime.now().strftime('%Y-%m-%d') 
         return context
